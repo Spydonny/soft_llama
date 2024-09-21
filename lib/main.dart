82cf88d76159bdf1f1bdcd43e9c2f49e1b136add
Spydonny <162360202+Spydonny@users.noreply.github.com>
2024-09-21 12:45:48 +0500

    ensureInitialized(

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:soft_llama/auth/auth_gate.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
